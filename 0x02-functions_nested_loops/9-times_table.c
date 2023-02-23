84d3373daaff0b871c955bbe2174f71462b9ab38
Hillarysidney <hillarysidney4@gmail.com>
2023-02-22 18:47:48 -0800

    Print

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j =0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
 			if (j == 0)
@@ -17,16 +17,15 @@ void times_table(void)
 				_putchar(k + '0');
 			}
 
-			if (k < 10 && j != 0);
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
-				_putchar('');
-				_putchar('');
-				_putchar(k + '0');
+				_putchar(' ');
+				_putchar(' ');
 			} else if (k >= 10)
 			{
 				_putchar(',');
-				_putchar('');
+				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
